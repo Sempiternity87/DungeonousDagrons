2aa5458cdbe5c4a6f68d0c71a493ba0c94734c69
Conrad Horton <conrad.horton@gmail.com>
2018-01-03 09:20:48 -0800

    Work on Dice app.
    Needs to be styled.

--- app/dice/dice.js
+++ app/dice/dice.js
@@ -2,17 +2,16 @@
   {
   "use strict"
 
-  /** Die object definition. */
+  /** Die object definition for reference. */
   var die =
     {
-    sides   : 0,
-    quantity: 0,
-    modifier: 0,
-    result  : 0,
-    image   : ""
+    sides   : 0, //Number of sides.
+    quantity: 0, //Quantity rolled.
+    modifier: 0, //+/- integer to modify roll by.
+    result  : 0, //(sides * qty) + modifier.
+    image   : "" //Image to use for button.
     };
 
-  
   angular.module("app")
  
   /*****************************************************************************
@@ -45,32 +44,67 @@
     ***************************************************************************/
     mThis.$onInit = function()
       {
+      /** Load the dice list. */
       mThis.dice = 
         [
-        {sides: 100, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d100.png"},
-        {sides: 20, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d20.png"},
-        {sides: 12, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d12.png"},
-        {sides: 10, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d10.png"},
-        {sides: 8, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d8.png"},
-        {sides: 6, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d6.png"},
-        {sides: 4, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d4.png"}
+        {sides: 100, quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d100.png"},
+        {sides: 20,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d20.png"},
+        {sides: 12,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d12.png"},
+        {sides: 10,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d10.png"},
+        {sides: 8,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d8.png"},
+        {sides: 6,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d6.png"},
+        {sides: 4,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d4.png"}
         ];
       };
       
     /***************************************************************************
-    * $onInit */
+    * roll */
     /**
-    * Initialize the controller.
+    * Generate random number based on passed in die object.
+    *
+    * @param  die  Die object to roll.
     ***************************************************************************/
     mThis.roll = function(die)
       {
-      var i = 0;
-      for(; i < die.quantity; i++)
+      var i      = 0;
+      var result = 0;
+      var sides  = Number(die.sides);
+      var qty    = Number(die.quantity);
+      
+      die.result = 0;
+      
+      for(; i < qty; i++)
         {
-        if(die.sides == 20)
-          die.result += chance.d20();
+        /** D100 */
+        if(sides == 100)
+          result += Number(chance.d100());
+        
+        /** D20 */
+        if(sides == 20)
+          result += Number(chance.d20());
+        
+        /** D12 */
+        if(sides == 12)
+          result += Number(chance.d12());
+        
+        /** D10 */
+        if(sides == 10)
+          result += Number(chance.d10());
+        
+        /** D8 */
+        if(sides == 8)
+          result += Number(chance.d8());
+        
+        /** D6 */
+        if(sides == 6)
+          result += Number(chance.d6());
+        
+        /** D4 */
+        if(sides == 4)
+          result += Number(chance.d4());
         }
-      die.result += die.modifier;
+      
+      die.result += Number(die.modifier) + result;
       }
       
     }//END DiceController
