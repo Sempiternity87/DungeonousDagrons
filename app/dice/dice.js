c24dc42f53a879f8dd79f0de65f8c9c947c5fdb5
Conrad Horton <conrad.horton@gmail.com>
2018-01-02 18:59:24 -0800

    Work on dice app.

--- app/dice/dice.js
+++ app/dice/dice.js
@@ -1,9 +1,20 @@
 (function()
   {
   "use strict"
+
+  /** Die object definition. */
+  var die =
+    {
+    sides   : 0,
+    quantity: 0,
+    modifier: 0,
+    result  : 0,
+    image   : ""
+    };
+
   
   angular.module("app")
-  
+ 
   /*****************************************************************************
   * Component: dice */
   /**
@@ -23,7 +34,10 @@
   function DiceController($scope)
     {
     /** Reference to self. */ var mThis = this;
-    
+
+    /** List of dice. */
+    mThis.dice = [];
+       
     /***************************************************************************
     * $onInit */
     /**
@@ -31,7 +45,33 @@
     ***************************************************************************/
     mThis.$onInit = function()
       {
-      
+      mThis.dice = 
+        [
+        {sides: 100, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d100.png"},
+        {sides: 20, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d20.png"},
+        {sides: 12, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d12.png"},
+        {sides: 10, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d10.png"},
+        {sides: 8, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d8.png"},
+        {sides: 6, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d6.png"},
+        {sides: 4, quantity: 0, modifier: 0, result: 0, image: "app/assets/images/d4.png"}
+        ];
       };
-    }
+      
+    /***************************************************************************
+    * $onInit */
+    /**
+    * Initialize the controller.
+    ***************************************************************************/
+    mThis.roll = function(die)
+      {
+      var i = 0;
+      for(; i < die.quantity; i++)
+        {
+        if(die.sides == 20)
+          die.result += chance.d20();
+        }
+      die.result += die.modifier;
+      }
+      
+    }//END DiceController
   })();
