edd502dc1afb1986f4570b9bb8ebba272c672674
Conrad Horton <conrad.horton@gmail.com>
2018-01-16 10:45:24 -0800

    Initiative tracker finished.
    Initial functionality complete. I can still add things like validation,
    but base functionality is there.

--- app/routes/initiativeTracker/initiativeTracker.js
+++ app/routes/initiativeTracker/initiativeTracker.js
@@ -23,20 +23,21 @@
   function InitiativeTrackerController($scope)
     {
     /** Reference to self. */ var mThis = this;
-    var id = 0;
     
-    /** Definition for an object on the grid. */
+    /** Tracks global ID for assigning to list objects. */ var currentID = 0;
+    
+    /** Definition for a character object on the table. */
     mThis.character =
       {
-      name: "", //Name of character.
-      init: 0,  //Initiative.
-      hp: 0,    //Health Points.
-      ac: 0,    //Armor Class.
-      touch: 0, //Touch AC.
-      bab: 0,   //Base Attack Bonus.
-      cmb: 0,   //Combat Maneuver Bonus.
-      cmd: 0,   //Combat Maneuver Defense.
-      id: 0     //ID of object.
+      name: ""  //Name of character.
+      ,init:  0 //Initiative.
+      ,hp:    0 //Health Points.
+      ,ac:    0 //Armor Class.
+      ,touch: 0 //Touch AC.
+      ,bab:   0 //Base Attack Bonus.
+      ,cmb:   0 //Combat Maneuver Bonus.
+      ,cmd:   0 //Combat Maneuver Defense.
+      ,id:    0 //ID of object.
       };
     
     /** List of characters in the table.*/
@@ -49,8 +50,8 @@
     **************************************************************************/
     mThis.$onInit = function()
       {
-      id++;
-      mThis.characterList.push({id: id});
+      currentID++;
+      mThis.characterList.push({id: currentID});
       };
       
     /**************************************************************************
@@ -60,20 +61,60 @@
     **************************************************************************/
     mThis.add = function()
       {
-      id++;
-      mThis.characterList.push({id: id});
+      currentID++;
+      mThis.characterList.push({id: currentID});
       };
       
     /**************************************************************************
     * down */
     /**
-    * Moves the object one down in the list.
+    * Moves the object one down in the list by increasing its index and
+    * swapping it with the object in the next highest index.
     **************************************************************************/
-    mThis.down = function(id)
+    mThis.down = function(character)
       {
-      console.log(id + " down");
+      var index = mThis.characterList.indexOf(character);
+      
+      /** If at the end of the list, do nothing. */
+      if(index + 1 < mThis.characterList.length)
+        {
+        var temp  = mThis.characterList[index];
+        var temp2 = mThis.characterList[index + 1];
+      
+        mThis.characterList[index]     = temp2;
+        mThis.characterList[index + 1] = temp;
+        }
       };
 
+    /**************************************************************************
+    * orderByInitiative */
+    /**
+    * Use bubble sort to order the list by initiative descending.
+    **************************************************************************/
+    mThis.orderByInitiative = function()
+      {
+      var swapped = false;
+      
+      do
+        {
+        swapped = false;
+        
+        for(var i = 0; i < mThis.characterList.length - 1; i++)
+          {
+          if(Number(mThis.characterList[i].init) < Number(mThis.characterList[i+1].init))
+            {
+            var temp = mThis.characterList[i];
+            
+            mThis.characterList[i]     = mThis.characterList[i + 1];
+            mThis.characterList[i + 1] = temp;
+            
+            swapped = true;
+            }
+          }
+        }
+      while(swapped);
+      };
+      
     /**************************************************************************
     * remove */
     /**
@@ -87,11 +128,22 @@
     /**************************************************************************
     * up */
     /**
-    * Moves the object one up in the list.
+    * Moves the object one up in the list by decreasing its index and
+    * swapping it with the object in the next lowest index.
     **************************************************************************/
-    mThis.up = function(id)
+    mThis.up = function(character)
       {
-      console.log(id + " up");
+      var index = mThis.characterList.indexOf(character);
+      
+      /** If at the end of the list, do nothing. */
+      if(index + 1 > 0)
+        {
+        var temp  = mThis.characterList[index];
+        var temp2 = mThis.characterList[index - 1];
+      
+        mThis.characterList[index]     = temp2;
+        mThis.characterList[index - 1] = temp;
+        }
       };
     }//END InitiativeTrackerController
   })();
