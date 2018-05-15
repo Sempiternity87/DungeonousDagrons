04147af6bb38a4c7cea9a6f464a7b38c8fa0408c
Sempiternity87 <sempiternity87@gmail.com>
2018-05-15 13:23:25 -0700

    Source files missing from automated push.

---
+++ DungeonousDagron/src/app/routes/characterSheet5e/characterSheet5e.js
@@ -0,0 +1,41 @@
+(function()
+  {
+  "use strict"
+
+  angular.module("app")
+
+  /*****************************************************************************
+  * Component: CharacterSheet */
+  /**
+  * Defines the component for the Character Sheet.
+  *****************************************************************************/
+  .component("characterSheet5e",
+    {
+    templateUrl:"app/routes/characterSheet5e/characterSheet5e.html",
+    controller: characterSheet5eController
+    });
+
+  /*****************************************************************************
+  * CharacterSheetController */
+  /**
+  * Controller for the Character Sheet component.
+  *****************************************************************************/
+  function characterSheet5eController($http, $scope, Character5e)
+    {
+    /** Reference to self. */ var mThis = this;
+
+
+    $http.get("app/assets/characters/Rex Darwin.json").then(function(resp)
+      {
+      var character = new Character5e(resp.data);
+      console.log(character);
+
+      mThis.character  = character;
+      }, function(err)
+      {
+      console.log(err)
+      });
+
+
+    }//END CharacterSheetController
+  })();
---
+++ DungeonousDagron/src/app/routes/dice/dice.js
@@ -0,0 +1,85 @@
+(function()
+  {
+  "use strict"
+
+  /** Die object definition for reference. */
+  var die =
+    {
+    sides   : 0, //Number of sides.
+    quantity: 0, //Quantity rolled.
+    modifier: 0, //+/- integer to modify roll by.
+    result  : 0, //(sides * qty) + modifier.
+    image   : "" //Image to use for button.
+    };
+
+  angular.module("app")
+
+  /*****************************************************************************
+  * Component: dice */
+  /**
+  * Defines the component for dice.
+  *****************************************************************************/
+  .component("dice",
+    {
+    templateUrl:"app/routes/dice/dice.html",
+    controller: DiceController
+    });
+
+  /*****************************************************************************
+  * DiceController */
+  /**
+  * Controller for the Dice component.
+  *****************************************************************************/
+  function DiceController($scope)
+    {
+    /** Reference to self. */ var mThis = this;
+
+    /** List of dice. */
+    mThis.dice = [];
+
+    /***************************************************************************
+    * $onInit */
+    /**
+    * Initialize the controller.
+    ***************************************************************************/
+    mThis.$onInit = function()
+      {
+      /** Load the dice list. */
+      mThis.dice =
+        [
+        {sides: 100, quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d100.png"},
+        {sides: 20,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d20.png"},
+        {sides: 12,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d12.png"},
+        {sides: 10,  quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d10.png"},
+        {sides: 8,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d8.png"},
+        {sides: 6,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d6.png"},
+        {sides: 4,   quantity: 1, modifier: 0, result: 0, image: "app/assets/images/d4.png"}
+        ];
+      };
+
+    /***************************************************************************
+    * roll */
+    /**
+    * Generate random number based on passed in die object.
+    *
+    * @param  die  Die object to roll.
+    ***************************************************************************/
+    mThis.roll = function(die)
+      {
+      var i      = 0;
+      var result = 0;
+      var sides  = Number(die.sides);
+      var qty    = Number(die.quantity);
+
+      die.result = 0;
+
+      for(; i < qty; i++)
+        {
+        result += Number(chance["d"+ sides]());
+        }
+
+      die.result += Number(die.modifier) + result;
+      }
+
+    }//END DiceController
+  })();
