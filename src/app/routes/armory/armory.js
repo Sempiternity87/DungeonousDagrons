32b7ce6169275d1adc2cebedc5319788f6b23eee
Sempiternity87 <sempiternity87@gmail.com>
2018-05-15 10:08:48 -0700

    Modified to run on ghpages

---
+++ src/app/routes/armory/armory.js
@@ -0,0 +1,20 @@
+(function()
+  {
+  "use strict";
+
+  angular.module('app').component('armory', Armory());
+
+  function Armory()
+    {
+    return 0,
+      {
+      templateUrl:'app/routes/armory/armory.html',
+      controller:ArmoryController
+      };
+    }
+
+  function ArmoryController()
+    {
+    var mThis = this;
+    }
+  })();
